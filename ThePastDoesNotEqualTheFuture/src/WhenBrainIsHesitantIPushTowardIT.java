28cd359031ecc29992964289d1bfbbbcd68b72fe
johnnyft172 <45769359+johnnyft172@users.noreply.github.com>
2018-12-18 21:26:15 -0500

    My 2 cents

--- ThePastDoesNotEqualTheFuture/src/WhenBrainIsHesitantIPushTowardIT.java
+++ ThePastDoesNotEqualTheFuture/src/WhenBrainIsHesitantIPushTowardIT.java
@@ -5,7 +5,9 @@ public class WhenBrainIsHesitantIPushTowardIT {
 
 		System.out.println("When my brain is resistant then I know there is opportunity for growth so I push past the pain "
 				+ "until I cross the 40 percent threshold then I find my 2nd wind and so I build momentum and I began to enjoy "
-				+ "what I once I was resistant and fearful of now I love it, I grew to love it!!");
+				+ "what I once I was resistant and fearful of now I love it, I grew to love it!!" + "Awesome 
+				  philosphy and I like to add that we need to do this everyday, committed to ligitimate pain
+				  every day of our lives!!!!!!);
 	}
 
 }
