f43b94ca1cfb18228673685faf1a53e267e5b0a9
jmintegr1 <jmintegr1@gmail.com>
2018-12-18 21:21:08 -0500

    First commit from Mack Intellij

---
+++ ThePastDoesNotEqualTheFuture/src/WhenBrainIsHesitantIPushTowardIT.java
@@ -0,0 +1,11 @@
+
+public class WhenBrainIsHesitantIPushTowardIT {
+
+	public static void main(String[] args) {
+
+		System.out.println("When my brain is resistant then I know there is opportunity for growth so I push past the pain "
+				+ "until I cross the 40 percent threshold then I find my 2nd wind and so I build momentum and I began to enjoy "
+				+ "what I once I was resistant and fearful of now I love it, I grew to love it!!");
+	}
+
+}
